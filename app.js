d22138a97b7e1d8860abb4d45591e6d06078feb8
AbrahamMartinez <612110359@utsalamanca.edu.mx>
2023-03-07 13:29:30 -0600

    Guardando cambios

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const cargaAnime = () => {
-
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'f63c2fae4emshdf22c9a1c98066dp11628cjsnce1971710d0e',
+            'X-RapidAPI-Host': 'anime-jokes.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://anime-jokes.p.rapidapi.com/animes/', options)
+        .then(response => response.json())
+        .then(response => {
+            news = response
+            console.log(response)
+        })
+        .catch(err => console.error(err));
 }
